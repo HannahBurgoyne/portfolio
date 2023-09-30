d4c57932957a363bdd148c5641a45843b39bad06
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2023-09-30 13:20:20 +1300

    Add placeholder content

--- client/components/About.tsx
+++ client/components/About.tsx
@@ -1,3 +1,27 @@
 export default function About() {
-  return <h3 className="text-white">About me</h3>
+  return (
+    <section className="p-10">
+      <h2 className="text-white text-center p-4">About me</h2>
+      <p className="text-white">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras turpis
+        quam, gravida at volutpat eu, laoreet sit amet ante. Sed vestibulum quis
+        ligula ac mollis. Phasellus eu suscipit eros. Etiam risus sem, laoreet a
+        ultrices at, volutpat eget libero. Donec dignissim in metus sed euismod.
+        Donec nec malesuada odio. Phasellus dapibus lorem tellus, aliquet
+        faucibus odio ornare volutpat. Aliquam a convallis lorem. Phasellus
+        pretium suscipit mattis. Sed pellentesque mi ut odio aliquam lobortis.
+        Vestibulum nec ante metus. Vestibulum scelerisque commodo dignissim.
+        Mauris iaculis risus posuere nisi ultricies, id bibendum dui dictum. Sed
+        sed ipsum ipsum. In sit amet hendrerit tortor, id fringilla est. Vivamus
+        maximus, dui at finibus vulputate, enim magna elementum enim, eget
+        laoreet ligula diam quis lectus. Phasellus facilisis metus vitae finibus
+        vehicula. Donec sit amet felis ullamcorper, pharetra sapien id, luctus
+        libero. Vivamus ultrices maximus metus id lacinia. Maecenas ornare est
+        elit, sit amet scelerisque augue rhoncus vitae. Curabitur id pulvinar
+        lorem. Pellentesque ultricies at libero id vehicula. Vivamus risus arcu,
+        bibendum in blandit commodo, pellentesque vitae mauris. Ut lectus justo,
+        interdum in pretium et, porttitor vitae magna. Nunc porta auctor porta.
+      </p>
+    </section>
+  )
 }
