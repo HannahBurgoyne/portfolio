d3bc943d36f1b2e4fdf4bc04d9a48358919a496f
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2023-09-30 13:20:38 +1300

    Adjust styling

--- client/components/UI/Container.tsx
+++ client/components/UI/Container.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export default function Container(props: Props) {
   return (
-    <section className="border-solid rounded border-white border-2 m-8 mx-32 flex items-center justify-center">
+    <section className="border-solid rounded border-[#333333] shadow-lg shadow-pink-700/40 border-2 m-8 mx-32 flex items-center justify-center">
       {props.children}
     </section>
   )
