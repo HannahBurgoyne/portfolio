669a8176c114d9ab0e4febfd16b33089de2868e2
Hannah <hannah.burgoyne@gmail.com>
2023-10-09 10:57:35 +1300

    add links and logos

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -1,9 +1,17 @@
 export default function Header() {
   return (
-    <header>
+    <header className="mx-auto max-w-screen-xl">
       <ul className="text-white list-none flex justify-between items-center mx-20 ">
-        <li>Github</li>
-        <li>LinkedIn</li>
+        <li>
+          <a href="https://github.com/HannahBurgoyne">
+            <img src="assets/github-mark-white.png" className="w-12" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/hannah-burgoyne-1311b5160/">
+            <img src="assets/In-White-128.png" className="w-10" />
+          </a>
+        </li>
         <li>Contact</li>
       </ul>
     </header>
