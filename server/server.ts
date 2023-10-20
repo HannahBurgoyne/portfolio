056a5835a61216a5b6ba301552c55aeeca8b8da3
Hannah <hannah.burgoyne@gmail.com>
2023-10-20 14:44:46 +1300

    Add routing to projects API

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,13 @@
 import * as Path from 'node:path'
-
+import * as URL from 'node:url'
+import * as fsPromises from 'node:fs/promises'
 import express from 'express'
+import projects from './projects'
 
 const server = express()
 
 server.use(express.json())
+server.use('/api/v1/projects', projects)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
