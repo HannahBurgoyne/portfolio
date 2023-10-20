53d8a78013b7e75658305e70ce409fffb2b7076a
Hannah <hannah.burgoyne@gmail.com>
2023-10-20 14:44:30 +1300

    Serve all projects data to client

---
+++ server/projects.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import * as Path from 'node:path'
+import * as URL from 'node:url'
+import * as fsPromises from 'node:fs/promises'
+
+const router = express.Router()
+
+const __filename = URL.fileURLToPath(import.meta.url)
+const __dirname = Path.dirname(__filename)
+
+router.get('/', async (req, res) => {
+  try {
+    const filePath = Path.join(__dirname, 'data', 'data.json')
+    const data = await fsPromises.readFile(filePath, 'utf-8')
+    res.json(data)
+
+    res.send(data)
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).send(error.message)
+    }
+  }
+})
+
+export default router
