14a5d9af52d463c56ee498f9e88d290a502a75c3
Hannah <hannah.burgoyne@gmail.com>
2023-10-20 16:02:07 +1300

    Rewrite projects summary

--- client/components/Projects.tsx
+++ client/components/Projects.tsx
@@ -10,51 +10,27 @@ export default function Projects() {
       <section className="p-10">
         <h2 className="text-white text-center p-4">Projects</h2>
         <p className="text-white">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras turpis
-          quam, gravida at volutpat eu, laoreet sit amet ante. Sed vestibulum
-          quis ligula ac mollis. Phasellus eu suscipit eros. Etiam risus sem,
-          laoreet a ultrices at, volutpat eget libero. Donec dignissim in metus
-          sed euismod. Donec nec malesuada odio. Phasellus dapibus lorem tellus,
-          aliquet faucibus odio ornare volutpat. Aliquam a convallis lorem.
-          Phasellus pretium suscipit mattis. Sed pellentesque mi ut odio aliquam
-          lobortis. Vestibulum nec ante metus. Vestibulum scelerisque commodo
-          dignissim. Mauris iaculis risus posuere nisi ultricies, id bibendum
-          dui dictum. Sed sed ipsum ipsum. In sit amet hendrerit tortor, id
-          fringilla est. Vivamus maximus, dui at finibus vulputate, enim magna
-          elementum enim, eget laoreet ligula diam quis lectus. Phasellus
-          facilisis metus vitae finibus vehicula. Donec sit amet felis
-          ullamcorper, pharetra sapien id, luctus libero. Vivamus ultrices
-          maximus metus id lacinia. Maecenas ornare est elit, sit amet
-          scelerisque augue rhoncus vitae. Curabitur id pulvinar lorem.
-          Pellentesque ultricies at libero id vehicula. Vivamus risus arcu,
-          bibendum in blandit commodo, pellentesque vitae mauris. Ut lectus
-          justo, interdum in pretium et, porttitor vitae magna. Nunc porta
-          auctor porta.
+          This is a selection of projects I've worked on recently. Please click
+          on a project to see further details and links.
         </p>
+
         <div className="grid gap-1 grid-cols-2">
           {data &&
             data.projects?.map((project) => (
-              <div>
-                <h2 key={project.id}>{project.name}</h2>
-                <ImgContainer>
-                  <img className="h-4" src={`${project.image}`} />
-                  <p className="text-white">Image caption</p>
-                </ImgContainer>
-              </div>
+              <ImgContainer>
+                <div key={project.id}>
+                  <h2 className="text-white" key={project.id}>
+                    {project.name}
+                  </h2>
+                  <img
+                    key={project.id}
+                    className="w-4"
+                    src={`${project.image}`}
+                  />
+                </div>
+              </ImgContainer>
             ))}
         </div>
-        {/* <ImgContainer>
-          <img className="h-4" src="#" />
-          <p className="text-white">Image caption</p>
-        </ImgContainer>
-        <ImgContainer>
-          <img className="h-4" src="#" />
-          <p className="text-white">Image caption</p>
-        </ImgContainer>
-        <ImgContainer>
-          <img className="h-4" src="#" />
-          <p className="text-white">Image caption</p>
-        </ImgContainer> */}
       </section>
     </>
   )
