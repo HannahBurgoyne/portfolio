c2864d0acd2fe690313b3ef10c297ca64d80963f
Hannah <hannah.burgoyne@gmail.com>
2023-10-24 15:21:33 +1300

    Match background to parent container background

--- client/components/UI/ImgContainer.tsx
+++ client/components/UI/ImgContainer.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export default function ImgContainer(props: Props) {
   return (
-    <section className="border-solid rounded-md border-[#333333] bg-black flex flex-col border-[1px] m-4 p-4 flex items-center justify-center hover:border-pink-700/40 hover:shadow-pink-700/40 hover:shadow-lg transition duration-300">
+    <section className="border-solid rounded-md border-[#333333] bg-slate-900 flex flex-col border-[1px] m-4 p-4 items-center justify-center hover:border-pink-700/40 hover:shadow-pink-700/40 hover:shadow-lg transition duration-300">
       {props.children}
     </section>
   )
