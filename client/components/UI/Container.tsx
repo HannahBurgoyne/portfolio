847878f774f7184908dc30ce88de6042fb72f5b7
Hannah <hannah.burgoyne@gmail.com>
2023-10-24 13:52:39 +1300

    Fix border width

--- client/components/UI/Container.tsx
+++ client/components/UI/Container.tsx
@@ -9,10 +9,10 @@ interface Props {
 export default function Container(props: Props) {
   return (
     <section
-      className={`relative m-10 overflow-hidden rounded-md border border-slate-800 p-[1px] backdrop-blur-3xl`}
+      className={`relative m-10 overflow-hidden rounded-md border border-transparent p-[1px] backdrop-blur-3xl`}
     >
       <span className="absolute inset-[-1000%]  animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#fce7f3_0%,#be185d_50%,#fce7f3_100%)]" />
-      <div className="inline-flex h-full w-full items-center justify-center rounded-md bg-slate-950 px-3 py-1 text-white backdrop-blur-3xl">
+      <div className="inline-flex h-full w-full m-[0.8px] items-center justify-center rounded-md bg-slate-900 px-6 py-1 text-white backdrop-blur-3xl">
         {props.children}
       </div>
     </section>
