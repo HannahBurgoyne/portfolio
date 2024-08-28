e1c8e36d01d1fec93eb6a990c2b778bb51751da6
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-08-28 15:28:08 +1200

    adjust label spacing

--- client/components/UI/Label.tsx
+++ client/components/UI/Label.tsx
@@ -6,7 +6,7 @@ export default function Button(props: Props) {
   const { children } = props
 
   return (
-    <li className="rounded-2xl border flex items-center justify-center border-[#333333] p-2 w-auto mx-2 bg-slate-950 min-width-fit">
+    <li className="rounded-2xl border flex items-center justify-center border-[#333333] p-2 w-auto mx-1 bg-slate-950 min-width-fit">
       {children}
     </li>
   )
